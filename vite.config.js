9bd864085fbb7a04130df8306edfd796f54795ee
code-abdulrehman <hy.abdulrahman2@gmail.com>
2024-08-26 15:09:24 +0500

    vercel 400 error

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 8080, 
     proxy: {
       '/api': {
-        target: 'https://bb.vdev.tech',
+        target: 'https://bb.vdev.tech/api',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '/api'),
