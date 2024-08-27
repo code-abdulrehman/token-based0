735ccc050190abc1844933a36452911c998e9b17
code-abdulrehman <hy.abdulrahman2@gmail.com>
2024-08-27 18:58:46 +0500

    simple backPage

--- src/Components/Pages/Auth/AuthController/index.jsx
+++ src/Components/Pages/Auth/AuthController/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Tabs, Tab, Card, CardBody } from "@nextui-org/react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { FaLock } from "react-icons/fa";
+import { FaChevronCircleLeft, FaLock } from "react-icons/fa";
 
 import Login from "./Login";
 import TokenControl from "./TokenControl";
@@ -32,7 +32,7 @@ export default function AuthController() {
     const currentTab = new URLSearchParams(location.search).get("tab");
     if (currentTab) {
       setSelected(currentTab);
-    } 
+    }
   }, [location.search, authToken]);
 
   useEffect(() => {
@@ -41,9 +41,9 @@ export default function AuthController() {
       case "login":
         document.title = `Login | ${appTitle}`;
         break;
-        case "signin":
-          document.title = `SignIn | ${appTitle}`;
-          break;
+      case "signin":
+        document.title = `SignIn | ${appTitle}`;
+        break;
       case "logout":
         document.title = `Logout | ${appTitle}`;
         break;
@@ -79,7 +79,7 @@ export default function AuthController() {
 
   const handleOtpVerification = async (values, { setErrors }) => {
     try {
-      const otp = values.otp
+      const otp = values.otp;
       await dispatch(otpVerify({ otp, email }));
       setResetTabs([passwordTabs[2]]); // Only set the Password Reset tab
     } catch (error) {
@@ -116,6 +116,20 @@ export default function AuthController() {
 
   const filteredTabs = !resetTabOpen ? tabs.filter(tab => tab.tokenBased === Boolean(authToken)) : resetTabs;
 
+  const backPage = (tab) => {
+    if(tab.key === 'reset-request'){
+      setResetTabOpen(false)
+    }
+    else if(tab.key === 'otp-verification'){
+      setResetTabs([passwordTabs[0]])
+    }
+    else if(tab.key === 'password-reset'){
+      setResetTabs([passwordTabs[1]])
+    }else{
+      setResetTabOpen(false)
+    }
+  }
+
   return (
     <div className="flex items-center justify-center rounded-xl min-h-[88vh] overflow-auto bg-gray-100 p-4 w-full">
       <Card className="max-w-full w-[340px] h-[400px]">
@@ -132,15 +146,33 @@ export default function AuthController() {
                 {tab.component}
                 {tab.key === 'login' && !resetTabOpen && (
                   <div className="flex justify-center items-center gap-4 py-2 text-sx">
-                    Reset 
-                    <b 
+                    Reset
+                    <b
                       className="underline cursor-pointer hover:text-blue-600"
                       onClick={() => {
                         setResetTabOpen(true);
                         setResetTabs([passwordTabs[0]]); // Open reset request tab
                       }}
                     >
-                      Password  <FaLock className="bounce"/>
+                      Password  <FaLock className="bounce" />
+                    </b>
+                  </div>
+                )}
+                {(tab.key === 'reset-request' || tab.key === 'otp-verification' || tab.key === 'password-reset') && resetTabOpen && (
+                  <div className="absolute bottom-0 flex justify-center items-center gap-4 py-2 text-sx">
+                    <FaChevronCircleLeft />
+                    <b
+                      className="underline cursor-pointer hover:text-blue-600"
+                      onClick={() =>  backPage(tab)}
+                    >
+                      {tab.key === 'reset-request'
+                        ? 'Login'
+                        : tab.key === 'otp-verification'
+                          ? 'Reset Request'
+                          : tab.key === 'password-reset'
+                            ? 'OTP Verification'
+                            : ''
+                      }
                     </b>
                   </div>
                 )}
